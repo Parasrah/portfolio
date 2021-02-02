3b33276228e92c56da0d265280ea3baa410fcb5c
Parasrah <git@parasrah.com>
2021-02-01 17:27:09 -0700

    remove unecessary config from logical.nix

--- logical.nix
+++ logical.nix
@@ -5,28 +5,11 @@
         pkgs.callPackage ./default.nix { env = "prod"; };
     in
     {
-      networking = {
-        firewall = {
-          enable = true;
-          allowPing = true;
-          allowedTCPPorts = [ 22 80 443 ];
-          allowedUDPPorts = [ ];
-        };
-      };
-
-      services = {
-        nginx = {
-          enable = true;
-          recommendedGzipSettings = true;
-          recommendedOptimisation = true;
-          recommendedTlsSettings = true;
-          virtualHosts."parasrah.com" = {
-            forceSSL = true;
-            root = "${portfolio}/www";
-            sslCertificate = ./.secrets/ssl/parasrah_com.crt;
-            sslCertificateKey = ./.secrets/ssl/parasrah.key;
-          };
-        };
+      services.nginx.virtualHosts."parasrah.com" = {
+        forceSSL = true;
+        root = "${portfolio}/www";
+        sslCertificate = ./.secrets/ssl/parasrah_com.crt;
+        sslCertificateKey = ./.secrets/ssl/parasrah.key;
       };
     };
 }
